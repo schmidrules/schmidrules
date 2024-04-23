20342726078cbe73a903eca265d0e6ee53dacb9d
Peter Kofler <peter.kofler@code-cop.org>
2024-04-23 22:03:21 +0200

    Verify the XMI result using Approvals.

--- src/test/java/org/schmidrules/SchmidRulesXmiTest.java
+++ src/test/java/org/schmidrules/SchmidRulesXmiTest.java
@@ -1,17 +1,34 @@
 package org.schmidrules;
 
+import java.io.ByteArrayOutputStream;
+
+import org.approvaltests.Approvals;
+import org.approvaltests.core.Options;
+import org.approvaltests.core.Scrubber;
+import org.approvaltests.scrubbers.DateScrubber;
+import org.approvaltests.scrubbers.MultiScrubber;
+import org.approvaltests.scrubbers.RegExScrubber;
 import org.junit.Test;
 import org.schmidrules.configuration.ConfigurationLoader;
 import org.schmidrules.configuration.dto.ArchitectureDto;
 
 public class SchmidRulesXmiTest {
 
-    // TODO quick and dirty test with output and without assert
     @Test
     public void shouldCreateXmi() throws Exception {
         ConfigurationLoader configurationLoader = new ConfigurationLoader("schmid-sample.xml");
         ArchitectureDto architecture = configurationLoader.getArchitecture();
         SchmidRulesXmi schmidRulesXmi = new SchmidRulesXmi(architecture, "SCHMID-SAMPLE");
-        schmidRulesXmi.createXmi(System.out);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        schmidRulesXmi.createXmi(out);
+        
+        String xml = new String(out.toByteArray());
+        Approvals.verify(xml, approvalOptions);
     }
+    
+    Options approvalOptions = new Options(new MultiScrubber(
+        new RegExScrubber("EAID_[0-9a-fA-F]{8}_[0-9a-fA-F]{4}_[0-9a-fA-F]{4}_[0-9a-fA-F]{4}_[0-9a-fA-F]{12}", n -> "EAID_" + n),
+        new Scrubber[] { new DateScrubber("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}") }));
+
 }
